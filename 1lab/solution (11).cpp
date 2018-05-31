741ef91415982ef9771f974fad5eea7cc06a920b
KestutisDikinis <38879518+KestutisDikinis@users.noreply.github.com>
2018-06-01 00:18:24 +0300

    Update solution (11).cpp

--- 1lab/solution (11).cpp
+++ 1lab/solution (11).cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+//Duoti kvadratinės lygties koeficientai a,b,c. Raskite lygties sprendinius.
 int main() {
-    double a,b,c,disk,x1,x2;
+    double a,b,c,diskriminantas,x1,x2;
     cin>>a>>b>>c;
-    disk=sqrt((b*b)-(4*a*c));
-    if(disk>0){
-        x1=(-b-disk)/(2*a);
-        x2=(-b+disk)/(2*a);
+    diskriminantas = sqrt((b*b)-(4*a*c));
+    if(diskriminantas>0){
+        x1=(-b-diskriminantas)/(2*a);
+        x2=(-b+diskriminantas)/(2*a);
         if (x1>=x2){
             cout<<x2<<"_"<<x1;
-        }
-        else{
+        }else{
             cout<<x1<<"_"<<x2;
         }
-    }
-    else if (disk==0){
+    }else if (diskriminantas==0){
         x1=-b/(2*a);
         cout <<x1;
-    }
-    else {
+    }else {
         cout << "NO";
     }
 }
