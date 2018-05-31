e2ce5ac5117a9def1c59a44cfc205c84c1148e5b
KestutisDikinis <38879518+KestutisDikinis@users.noreply.github.com>
2018-06-01 00:15:52 +0300

    Update solution (10).cpp

--- 1lab/solution (10).cpp
+++ 1lab/solution (10).cpp
@@ -1,21 +1,20 @@
 #include <iostream>
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <iomanip>
-
 using namespace std;
-
 
 int main()
 {
-    double p, h, r, ats,pi = 3.14159265359;
+    double p, h, r, ats;
     cin >> p >> h >> r;
     if (p==1) {
-        ats = pi*r*r*h;
-        cout<< fixed<<setprecision(2)<< ats << endl;
+        ats = M_PI*r*r*h;
+        cout << fixed << setprecision(2) << ats << endl;
     }
     else if (p==2) {
-        ats = pi *r*r*h / 3;
-        cout<<fixed<< setprecision(2) << ats << endl;
+        ats = M_PI*r*r*h / 3;
+        cout << fixed << setprecision(2) << ats << endl;
     }
     return 0;
 }
